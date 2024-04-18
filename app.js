035a85cd8977f44bd85a91d321b5615cc6f07802
Alym100 <asymamasharipov09@gmail.com>
2024-04-18 15:46:02 +0300

    number

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const app=document.querySelector("button")
 const ann=document.querySelector("h4")
 
+
+app.onclick=()=>{
+    const random=Math.floor(Math.random()*100)
+    ann.innerHTML=random
+    if (random>30 && random<40){
+        alert("siz taptynyz")
+    }
+    
+}
